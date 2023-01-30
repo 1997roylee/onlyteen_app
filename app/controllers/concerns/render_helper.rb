cfecdadf3576bfe1112444b59e4d299a5164cc7a
Roy <53046899+1997roylee@users.noreply.github.com>
2023-01-30 11:28:56 +0800

    feat: add render helper

---
+++ app/controllers/concerns/render_helper.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# RenderHelper
+module RenderHelper
+  extend ActiveSupport::Concern
+
+  included do
+    helper_method :render_success
+    helper_method :render_errors
+    helper_method :render_json
+  end
+
+  def render_success(data)
+    render_json(data)
+  end
+
+  def render_errors(errors, status = :unprocessable_entity)
+    render_json({
+      errors:,
+      status:,
+    }, status)
+  end
+
+  def render_json(json, status = :ok)
+    render(json:, status:)
+  end
+end
