bbbc92515e9cff7a324d57afe5365a1dd718496f
Roy <53046899+1997roylee@users.noreply.github.com>
2023-01-30 11:42:14 +0800

    feat: custom users/session controller

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Users
+  class SessionsController < Devise::SessionsController
+    respond_to :json
+
+    private
+
+    def respond_with(_resource, _opts = {})
+      render_success({
+        message: "Signed in.",
+        resource: current_user,
+      })
+    end
+
+    def respond_to_on_destroy
+      current_user ? log_out_success : log_out_failure
+    end
+
+    def log_out_success
+      render(json: { message: "Logged out." }, status: :ok)
+    end
+
+    def log_out_failure
+      render(json: { message: "Logged out failure." }, status: :unauthorized)
+    end
+  end
+end
