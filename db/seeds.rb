05ec05b0b4d3a18022b15bcb0bb8f001fd90e539
Roy <53046899+1997roylee@users.noreply.github.com>
2023-01-30 11:59:03 +0800

    feat: add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+if Rails.env.development?
+  user = User.create!(
+    email: "demo@onlyteen.app",
+    password: "password",
+    password_confirmation: "password",
+  )
+
+  10.times do
+    Post.create!(
+      title: Faker::Lorem.sentence,
+      body: Faker::Lorem.paragraph,
+      user: user
+    )
+  end
+end
