4fa4eff13bb960ca373025eff595e5aaf805abe8
Roy <53046899+1997roylee@users.noreply.github.com>
2023-01-30 11:57:44 +0800

    feat: update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations",
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'home#index'
+  root "home#index"
+  get "/profile", to: "home#index"
+  get "/login", to: "home#index"
 end
