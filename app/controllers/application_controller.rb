8521b7eef5e9e631f4ba25e7b687e5560c5cf784
Roy <53046899+1997roylee@users.noreply.github.com>
2023-01-30 11:58:22 +0800

    feat: add before action configure permitted paramters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  include RenderHelper
+
+  def configure_permitted_parameters
+    #   devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
+  end
 end
