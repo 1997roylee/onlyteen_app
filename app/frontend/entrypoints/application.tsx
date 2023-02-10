c84a2883b1d5365f6f6a61796c8eac02bf7e03a1
Roy <53046899+1997roylee@users.noreply.github.com>
2023-02-11 01:09:59 +0800

    clean

--- app/frontend/entrypoints/application.tsx
+++ app/frontend/entrypoints/application.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "../App";
-// import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-console.log("root", root);
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// reportWebVitals();
