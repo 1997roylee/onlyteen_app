496697ee07dadadb4cfc628ba8cd3cb864b3b222
Roy <53046899+1997roylee@users.noreply.github.com>
2023-02-11 01:09:44 +0800

    create App

--- app/frontend/App.tsx
+++ app/frontend/App.tsx
@@ -1,5 +1,60 @@
-import React from 'react'
+import '@fontsource/outfit/400.css';
+import '@fontsource/outfit/300.css';
+import '@fontsource/outfit/700.css';
+import '@fontsource/outfit/600.css';
+import { theme } from '../../theme';
+import { ChakraBaseProvider } from '@chakra-ui/react';
+import loadable from '@loadable/component'
+import { StaticRouter } from "react-router-dom/server";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ToastContainer } from 'utils/toast';
+const Home = loadable(() => import("pages/Home"))
+const Profile = loadable(() => import("pages/Profile"))
+const Login = loadable(() => import("pages/Login"))
+
+const queryClient = new QueryClient()
+
+const routers = [
+    {
+        path: "/",
+        element: <Home />
+    },
+    {
+        path: '/profile',
+        element: <Profile />
+    },
+    {
+        path: '/login',
+        element: <Login />
+    }
+]
+
+const Router = () => {
+    if (window) {
+        return <BrowserRouter>
+            <Routes>
+                {
+                    routers.map((route) => <Route key={route.path} path={route.path} element={route.element} />)
+                }
+            </Routes>
+        </BrowserRouter>
+    }
+
+    return <StaticRouter location={""}>
+        <Routes>
+            {
+                routers.map((route) => <Route key={route.path} path={route.path} element={route.element} />)
+            }
+        </Routes>
+    </StaticRouter>
+}
 
 export default function App() {
-    return <div>1234</div>
+    return <ChakraBaseProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+            <Router />
+        </QueryClientProvider>
+        <ToastContainer />
+    </ChakraBaseProvider>
 }
