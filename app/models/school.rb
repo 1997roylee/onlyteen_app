f54768240afb26acfb6369f4ae40e08a23e3192b
Roy <1997roylee@gmail.com>
2023-02-12 22:21:21 -0800

    set enum city into School model

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,4 +2,10 @@
 
 class School < ApplicationRecord
   enum school_level: { middle_school: 1, college: 2, university: 3 }
+  enum city: {
+    "Macau" => 1,
+    "Hong Kong" => 2,
+    "Taiwan" => 3,
+    "Oversea" => 4,
+  }
 end
