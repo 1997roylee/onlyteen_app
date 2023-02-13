c2bb6b70243eeff3f6db62d647b0c32e7166a1c7
Roy <1997roylee@gmail.com>
2023-02-12 22:20:41 -0800

    change city to integer column and add index

--- db/migrate/20230213060239_create_schools.rb
+++ db/migrate/20230213060239_create_schools.rb
@@ -6,10 +6,11 @@ class CreateSchools < ActiveRecord::Migration[7.0]
       t.string(:name, null: false, default: "", limit: 100)
       t.string(:slug, null: false, default: "", limit: 100, index: { unique: true })
       t.string(:address, null: false, default: "", limit: 256)
-      t.string(:city, null: false, default: "", limit: 100)
+      t.integer(:city, null: false, default: 0)
       t.integer(:school_level, null: false, default: 0)
       t.timestamps
     end
     execute("ALTER SEQUENCE schools_id_seq RESTART WITH 10000")
+    add_index(:schools, :city)
   end
 end
