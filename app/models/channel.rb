d2512685809575cffd6d461acb4c79c2c221128d
Roy <1997roylee@gmail.com>
2023-02-12 22:17:12 -0800

    dump channel

--- app/models/channel.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Channel < ApplicationRecord
-  belongs_to :owner, class_name: "User", foreign_key: "owner_id"
-  has_many :posts, dependent: :destroy
-  # has_many :follows, dependent: :destroy
-end
